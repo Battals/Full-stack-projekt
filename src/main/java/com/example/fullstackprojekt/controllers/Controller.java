a650f94eb658650f3b68d356f06b52fb6631bb6e
Shaundator <rash0081@kea.stud.dk>
2022-03-28 10:20:12 +0200

    Commit1

--- src/main/java/com/example/fullstackprojekt/controllers/Controller.java
+++ src/main/java/com/example/fullstackprojekt/controllers/Controller.java
@@ -1,4 +1,22 @@
 package com.example.fullstackprojekt.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class Controller {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/rasha")
+    public String rasha(){
+        return "indexRasha";
+    }
+
+    @GetMapping("/battal")
+    public String battal(){
+        return "indexBattal";
+    }
+
 }
