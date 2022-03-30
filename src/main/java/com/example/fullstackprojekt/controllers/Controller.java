590ad29bc85201f9c425caf6a404f3f5db03b11e
Shaundator <rash0081@kea.stud.dk>
2022-03-30 11:30:34 +0200

    Commit8

--- src/main/java/com/example/fullstackprojekt/controllers/Controller.java
+++ src/main/java/com/example/fullstackprojekt/controllers/Controller.java
@@ -2,8 +2,10 @@ package com.example.fullstackprojekt.controllers;
 
 
 
+import com.example.fullstackprojekt.respositories.SQLManager;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 
 @org.springframework.stereotype.Controller
@@ -26,5 +28,19 @@ public class Controller {
     public String battal(){
         test = "battal";
         return "indexBattal";
+    }
+
+    @PostMapping("/logging")
+    public String logging(WebRequest dataFromForm){
+        SQLManager sql = new SQLManager();
+        sql.start();
+        try{
+            //"dataFromForm.getParameter("username")" = String
+            System.out.println(dataFromForm.getParameter("username"));
+            System.out.println(dataFromForm.getParameter("password"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
     }
 }
